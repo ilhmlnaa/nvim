55d2fe363392f0cf2ab8779eaf8ac319d0bdb631
ilhmlnaa <poloklasik32@gmail.com>
2024-07-26 10:20:11 +0700

    update

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,6 +1,18 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    git_status = {
+      -- Display Git status information
+      symbols = {
+        added = "✚",  -- Added files
+        modified = "✹",  -- Modified files
+        deleted = "✖",  -- Deleted files
+        renamed = "➜",  -- Renamed files
+        ignored = "☒",  -- Ignored files
+        untracked = "?",  -- Untracked files
+      },
+    },
+   
     filesystem = {
       filtered_items = {
         visible = true,
